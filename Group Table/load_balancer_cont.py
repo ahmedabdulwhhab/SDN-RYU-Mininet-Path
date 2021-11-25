f92af86ff974df358975cbc75a85a59119f1f701
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2021-11-25 09:59:32 +0200

    Update load_balancer_cont.py

--- Group Table/load_balancer_cont.py
+++ Group Table/load_balancer_cont.py
@@ -2,7 +2,7 @@
 #https://mail.openvswitch.org/pipermail/ovs-discuss/2016-August/042394.html
 #https://stackoverflow.com/questions/36949861/group-table-issue-openflow-mininet
 
-##clear && sudo ryu-manager load_balancer_cont.py ../../flowmanager/flowmanager.py  --observe-links --ofp-tcp-listen-port 6633
+##clear && sudo ryu-manager load_balancer_cont.py ../../flowmanager/flowmanager.py ryu.app.ofctl_rest --observe-links --ofp-tcp-listen-port 6633
 
 
 from ryu.base import app_manager
