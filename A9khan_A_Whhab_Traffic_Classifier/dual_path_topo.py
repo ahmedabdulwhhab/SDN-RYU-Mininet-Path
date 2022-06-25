97feb0ef13650e90b10635832bf7b82ddeb76e65
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2022-06-25 14:46:25 +0200

    Update dual_path_topo.py

--- A9khan_A_Whhab_Traffic_Classifier/dual_path_topo.py
+++ A9khan_A_Whhab_Traffic_Classifier/dual_path_topo.py
@@ -31,9 +31,9 @@ class SingleSwitchTopo(Topo):
         s4 = self.addSwitch('s4')
 
 
-        h1 = self.addHost('h1', mac="00:00:00:00:00:01", ip="192.168.1.1/24")
-        h2 = self.addHost('h2', mac="00:00:00:00:00:02", ip="192.168.1.2/24")
-        h3 = self.addHost('h3', mac="00:00:00:00:00:03", ip="192.168.1.3/24")
+        h1 = self.addHost('h1', mac="00:00:00:00:00:01", ip="10.0.0.1/24")
+        h2 = self.addHost('h2', mac="00:00:00:00:00:02", ip="10.0.0.2/24")
+        h3 = self.addHost('h3', mac="00:00:00:00:00:03", ip="10.0.0.3/24")
 
 
         self.addLink(s1,s2,1,2)
