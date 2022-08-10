6f53d06bdad620b095393efa3be56f9aa4e8ea02
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2022-08-10 08:05:14 +0200

    Add files via upload

---
+++ A9khan_A_Whhab_Traffic_Classifier/Generate_DNS_Dataset/timer_dns_to_h1.py
@@ -0,0 +1,17 @@
+import time
+import urllib.request
+import urllib.error
+import os
+
+
+def uptime_bot(msg):
+    while True:
+        try:
+            os.system("ITGSend script_file_1 -l sender_log_file_1")
+        except: 
+            print('Error')
+        time.sleep(20)
+
+if __name__ == '__main__':
+    msg = 'In the name of Allah'
+    uptime_bot(msg)
