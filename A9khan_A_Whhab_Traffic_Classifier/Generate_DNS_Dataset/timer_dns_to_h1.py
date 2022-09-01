a62f8f0cf5cb567f6533df0681bf849564805238
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2022-09-01 22:01:43 +0200

    Update timer_dns_to_h1.py

--- A9khan_A_Whhab_Traffic_Classifier/Generate_DNS_Dataset/timer_dns_to_h1.py
+++ A9khan_A_Whhab_Traffic_Classifier/Generate_DNS_Dataset/timer_dns_to_h1.py
@@ -10,7 +10,7 @@ def uptime_bot(msg):
             os.system("ITGSend script_file_1 -l sender_log_file_1")
         except: 
             print('Error')
-        time.sleep(20)
+        time.sleep(3)
 
 if __name__ == '__main__':
     msg = 'In the name of Allah'
