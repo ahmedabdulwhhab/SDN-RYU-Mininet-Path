91e75cddae0715045507e1fbd71935c0c926fe55
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2022-11-24 07:18:30 +0200

    Update my_monitor_006_Manual_DDOS_Recovery.py

--- DDOS_Detect_MANUAL_RECOVERY/my_monitor_006_Manual_DDOS_Recovery.py
+++ DDOS_Detect_MANUAL_RECOVERY/my_monitor_006_Manual_DDOS_Recovery.py
@@ -140,10 +140,10 @@ class SimpleSwitch13(app_manager.RyuApp):
                 # verify if we have a valid buffer_id, if yes avoid to send both
                 # flow_mod & packet_out
                 if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                    self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                    self.add_flow(datapath, 1, match, actions, msg.buffer_id, idle=50, hard=100*3)
                     return
                 else:
-                    self.add_flow(datapath, 1, match, actions)
+                    self.add_flow(datapath, 1, match, actions, idle=50, hard=100*3)
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
