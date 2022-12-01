f798d6d8be27aa27bd50caf9218aa713d452e087
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2022-12-01 06:53:30 +0200

    Update my_monitor_009_Manual_DDOS_Recovery.py

--- DDOS_Detect_MANUAL_RECOVERY/my_monitor_009_Manual_DDOS_Recovery.py
+++ DDOS_Detect_MANUAL_RECOVERY/my_monitor_009_Manual_DDOS_Recovery.py
@@ -193,29 +193,7 @@ class SimpleSwitch13(app_manager.RyuApp):
                                         )
                                         #############################
                 self.mac_ip_to_dp[src][ip.src] = 0                                         
-                if(len(self.mac_ip_to_dp[src]) > 5):
-                    self.ddos_oocurs=True
-                    print("DDos occur from src ", src)
-                    match1 = parser.OFPMatch( eth_dst=dst, eth_src=src)
-                    match2 = parser.OFPMatch( eth_src=src)     #block src only with low priority
-                    match3 = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP,
-                                            ip_proto=protocol,
-                                            eth_dst=dst, eth_src=src)
-                    self.add_flow(datapath, 114, match3, [],idle=30, hard=100*3)  					
-                    for dp in self.datapaths.values():
-                        if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                            self.add_flow(dp, 110, match1, [],msg.buffer_id, idle=30, hard=100*2)
-                            self.add_flow(dp, 108, match2, [],msg.buffer_id, idle=30, hard=100*2)
-                            self.add_flow(dp, 112, match3, [], msg.buffer_id, idle=30, hard=100*3) 							
-                        else:
-                            self.add_flow(dp, 110, match1, [],idle=30, hard=100*2)
-                            self.add_flow(dp, 108, match2, [], idle=30, hard=100*2)
-                            self.add_flow(dp, 112, match3, [],idle=30, hard=100*3) 							
-                    #import time
-                    #time.sleep(20)
-                    #print("sleep duration is finished")
-                    #return-2                                        
-                                        #############################
+
                 # verify if we have a valid buffer_id, if yes avoid to send both
                 # flow_mod & packet_out
                 if msg.buffer_id != ofproto.OFP_NO_BUFFER:
